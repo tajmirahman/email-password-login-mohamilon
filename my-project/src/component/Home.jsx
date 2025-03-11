b19b42063acdf897f5fd32b172a0cfb8848a9578
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-11 13:39:37 +0600

    login and register form start with context api

---
+++ my-project/src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center'>Login and Register Form Submit with Context Api</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ my-project/src/component/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="my-6">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-3xl font-bold">Login now!</h1>
+                    
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="fieldset-label">Email</label>
+                            <input type="email" name='email' className="input" placeholder="Email" />
+                            <label className="fieldset-label">Password</label>
+                            <input type="password" name='password' className="input" placeholder="Password" />
+
+                            <div><a className="link link-hover">Forgot password?</a></div>
+
+                            <button className="btn btn-neutral mt-4">Login</button>
+
+                            <p>If you don't have an account? Please <Link className='text-green-500 underline' to={'/register'}>register</Link></p>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
